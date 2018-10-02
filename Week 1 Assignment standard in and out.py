1fe1a3cb263ecb10c468d020dd124786c18ef4fc
David Paul <davidp765@hotmail.com>
2018-10-02 18:50:32 +0100

    David's first commit

---
+++ Week 1 Assignment standard in and out.py
@@ -0,0 +1,12 @@
+print('Lei Meis weekly assignment Standard in and out')
+print('01/10/2018')
+
+print('Hello I am fruit shop')
+
+customer=input("What is your name?: ") #not sure how to include ' in the I'll part as it closes my print
+print("Hello", customer, "what fruit would you like to buy?") #how do I put a comma after the Hello Lei Mei, what etc..
+
+
+fruit=input() #not sure what the difference is between "" and ''
+print ("You have bought:", fruit)
+
